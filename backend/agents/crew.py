f69170c3569a12f9999d3ab0f8d22d12e7291fe6
rithul17 <rithul1905@gmail.com>
2025-05-29 23:50:08 +0530

    path

--- backend/agents/crew.py
+++ backend/agents/crew.py
@@ -3,9 +3,8 @@ from crewai.project import CrewBase, agent, crew, task
 from dotenv import load_dotenv
 import yaml
 import os
-#import sys
-#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 load_dotenv()
 
 @CrewBase
